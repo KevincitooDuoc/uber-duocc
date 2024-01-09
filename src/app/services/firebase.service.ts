a65c439925a4f3e7d825596a5a034eacaf39b8e6
KevincitooDuoc <kevincarvajal.wolf@gmail.com>
2024-01-09 16:10:47 -0300

    Avance en el registro

---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, inject } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { User } from '../models/user.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  auth = inject(AngularFireAuth);
+
+  // ====================== Autenticacion ======================
+
+  // === Acceder ===
+
+  signIn(user: User) {
+    return signInWithEmailAndPassword(getAuth(), user.email, user.password);
+  }
+
+  // === Crear ===
+
+  signUp(user: User) {
+    return createUserWithEmailAndPassword(getAuth(), user.email, user.password);
+  }
+
+  // === Actualizar ===
+
+  updateUser(displayName: string) {
+    return updateProfile(getAuth().currentUser, { displayName });
+  }
+
+}
