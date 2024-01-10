8432e2d71825c5f08eb8413fe7c26e557f8ea4b0
KevincitooDuoc <kevincarvajal.wolf@gmail.com>
2024-01-09 22:08:32 -0300

    Cerrar sesión botón

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -4,6 +4,7 @@ import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, up
 import { User } from '../models/user.model';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { getFirestore, setDoc, doc, getDoc } from '@angular/fire/firestore';
+import { UtilsService } from './utils.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ export class FirebaseService {
 
   auth = inject(AngularFireAuth);
   firestore = inject(AngularFirestore);
+  utilsSvc = inject(UtilsService);
 
   // ====================== Autenticacion ======================
 
@@ -41,6 +43,14 @@ export class FirebaseService {
 
   sendRecoveryEmail(email: string) {
     return sendPasswordResetEmail(getAuth(), email);
+  }
+
+  // === Cerrar Sesion ===
+
+  signOut() {
+    getAuth().signOut();
+    localStorage.removeItem('user');
+    this.utilsSvc.routerLink('/auth');
   }
 
   // ====================== Base de datos ======================
