21b8beffb4326771dc455f4fa31aa1bea90f3644
KevincitooDuoc <kevincarvajal.wolf@gmail.com>
2024-01-11 21:13:41 -0300

    Interfaz lista ??

--- src/app/pages/main/conductor/modificar-viaje2/modificar-viaje2.page.ts
+++ src/app/pages/main/conductor/modificar-viaje2/modificar-viaje2.page.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modificar-viaje2',
   templateUrl: './modificar-viaje2.page.html',
   styleUrls: ['./modificar-viaje2.page.scss'],
 })
-export class ModificarViaje2Page implements OnInit {
+export class ModificarViaje2Page {
+  nombreLugar: string = '';
+  coste: string = '';
+  horaSalida: string = '';
+  asientos: string = '';
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {}
 
-  ngOnInit() {
+  guardarCambios() {
+    console.log('Nombre del Lugar:', this.nombreLugar);
+    console.log('Coste:', this.coste);
+    console.log('Hora de Salida:', this.horaSalida);
+    console.log('Asientos:', this.asientos);
+
   }
 
+  volver() {
+    this.navCtrl.back();
+  }
 }
+
