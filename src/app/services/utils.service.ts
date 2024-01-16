681092dc5f3ad50e76fd97dbdd0df66c1279a258
manu <ma.figueroap@duocuc.cl>
2024-01-16 11:22:24 -0300

    avance clase

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { LoadingController, ToastController, ToastOptions } from '@ionic/angular';
+import { LoadingController, ModalController, ModalOptions, ToastController, ToastOptions } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,7 @@ export class UtilsService {
   loadingCtrl = inject(LoadingController);
   toastCtrl = inject(ToastController);
   router = inject(Router);
+  modalCtrl = inject(ModalController);
 
   // === loading ===
   loading() {
@@ -34,5 +35,15 @@ export class UtilsService {
 
   getFromLocalStorage(key: string) {
     return JSON.parse(localStorage.getItem(key))
+  }
+
+  // Modal
+  async presentModal(opts:ModalOptions){
+    const modal = await this.modalCtrl.create(opts);
+    await modal.present();
+
+    const {data} = await modal.onWillDismiss();
+    if (data) return data;
+
   }
 }
